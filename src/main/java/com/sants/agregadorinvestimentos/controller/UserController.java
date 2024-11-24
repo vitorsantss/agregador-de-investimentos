895ee57452da2e74c56d8f812cf8bc3986f1aca7
Vitor Santos <santoss.coding@gmail.com>
2024-11-24 17:32:28 -0300

    Add UserController

---
+++ src/main/java/com/sants/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.sants.agregadorinvestimentos.controller;
+
+import com.sants.agregadorinvestimentos.entity.User;
+import com.sants.agregadorinvestimentos.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+        var userId = userService.createUser(createUserDto);
+
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
+        return null;
+    }
+}
